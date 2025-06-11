9fe8a3b7ebdf2669548b36e2d08fc91bedf73134
Roggerrs <rogger6412@gmail.com>
2025-06-11 16:11:41 -0300

    Create 11 06 2025 Horas 16:10 Post valor e DataHora

---
+++ src/main/java/br/com/feltex/desafioitau/transacao/TransacaoRequest.java
@@ -0,0 +1,19 @@
+package br.com.feltex.desafioitau.transacao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransacaoRequest {
+
+private BigDecimal valor;
+private OffsetDateTime dataHora;
+
+
+}
